0650f1ac6bc84ba322f1d938569e41f040309285
Chema <jmmuro88@gmail.com>
2022-11-22 16:57:30 +0100

    cambiosultimahora

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,12 +5,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import Value from "./value";
 
 <><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
-
+<link rel="stylesheet" href="./styles.css" />
 </>
 //create your first component
 const Home = () => {
 	return (
-		<div className="container"><h1 className="fo">To Do List</h1>
+		<div className="container"><h1 className="titular">To Do List</h1>
 		<div className="fondo">
 			<Value/>
 		</div>
